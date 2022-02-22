73d6b3b4c69a04c11630725fe9a5a4687bf621f8
billyberisha <67516144+billyberisha@users.noreply.github.com>
2022-02-22 06:06:34 -0500

    Add files via upload
    Week 3 exercise or comparing two cards ScaleFocus

---
+++ week3/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Choose a card from 1 to 14, and a suit for that card")
+	fmt.Println("Suits are as following: 4,3,2,1")
+	cardGame(36, 4, 16, 0)
+}
+
+func cardGame(cOneVal int, cOneSuit int, cTwoVal int, cTwoSuit int) int {
+	isValidCard := cOneVal > 1 && cOneVal < 15 || cTwoVal > 1 && cTwoVal < 15
+	isValidSuit := cOneSuit > 0 && cOneSuit < 5 || cOneVal > 0 && cOneVal < 5
+	if isValidCard && isValidSuit {
+		if cOneVal > cTwoVal && cOneSuit > cTwoSuit {
+			fmt.Println("Card one has won")
+			return (1)
+		} else if cOneVal < cTwoVal && cOneSuit < cTwoSuit {
+			fmt.Println("Card two has won")
+			return (-1)
+		} else {
+			fmt.Println("The game is even")
+			return (2)
+		}
+
+	} else {
+		fmt.Printf("The first card %v and the second card %v have to be in the range of 2 and 14\n", cOneVal, cTwoVal)
+		fmt.Printf("The first suit %v and the second suit %v have to be in the range of 1 and 4\n", cOneSuit, cTwoSuit)
+
+	}
+	return 1
+}
