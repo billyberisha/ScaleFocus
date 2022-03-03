f7a6414e3b66a62c6c3f88332403817d879c6348
billyberisha <67516144+billyberisha@users.noreply.github.com>
2022-03-03 08:40:03 -0500

    Update main.go

--- week2/task1/main.go
+++ week2/task1/main.go
@@ -2,26 +2,31 @@ package main
 
 import "fmt"
 
-func main() {
-	daysInMonth(3, 2016)
-}
-
 func daysInMonth(month int, year int) (int, bool) {
 	switch month {
 	case 1, 3, 5, 7, 8, 10, 12:
-		fmt.Println("31")
-		break
+		return 31, true
 	case 4, 6, 9, 11:
-		fmt.Println("30")
-		break
+		return 30, true
 	case 2:
-		if (month%4 == 0) && (month%100 != 0 || month%400 == 0) {
-			fmt.Println("29")
-		} else {
-			fmt.Println("28")
-		}
-		break
-
+		return checkFeb(year), true
+	default:
+		return -1, false
 	}
-	return 1, true
+}
+
+func checkFeb(year int) int {
+	if (year%4 == 0) && (year%100 != 0 || year%400 == 0) {
+		return 29
+	}
+	return 28
+}
+
+func main() {
+	if days, ok := daysInMonth(2, 2000); ok {
+		fmt.Println(days)
+	} else {
+		fmt.Println("Invalid month")
+	}
+
 }
